e21cc7e6cbf9533c39d8c474f315753b5642dcd1
iisyos <kosuke1216a@yahoo.co.jp>
2022-05-05 18:58:33 +0900

    WIP

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,17 +4,34 @@
 	import type { Unit as UnitType,Label } from '../../types'
 	import { unitList,shuffle, set} from '$lib/store';
 	import { onMount } from 'svelte';
+  import { MetaTags } from 'svelte-meta-tags';
+  import calendar from '$lib/calendar.jpg';
+
 onMount(()=>{
 	set()
 })
 
 </script>
+
 <div class='wrapper'>
 	{#each Array.from(Array(35)) as _,i}
 		<Unit day={i}/>
 	{/each}
 </div>
 
+<MetaTags
+  openGraph={{
+    type: 'website',
+    url: 'https://my-calendar-today.com/',
+    title: 'My Calendar Today',
+    images: [
+      {
+        url: calendar,
+        alt: 'Og Image Alt'
+      }
+    ]
+  }}
+/>
 
 
 <style>
